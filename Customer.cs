f69de0bde52e9ebeeb9214b1e3099dc846bfdc6f
unknown <peterone23four@gmail.com>
2021-06-02 16:10:57 +0530

    commit to repos

---
+++ Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleapplication
+{
+    class Customer
+    {
+
+         //HACK:Please use parameters 
+          public void Accept()
+        {
+           
+        }
+
+        //BUG:Please correct this
+        public void Delete()
+        {
+
+        }
+    }
+}
