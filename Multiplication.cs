46a4c6be8d2fe610af4419afad6651b75b3691be
devopsasp <85219013+devopsasp@users.noreply.github.com>
2021-06-02 13:51:05 +0530

    Add files via upload

---
+++ Multiplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+  public  class Multiplication
+    {
+
+        public void Multiply()
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i * j);
+
+        }
+    }
+}
