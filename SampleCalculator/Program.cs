b2d4672aaffa98c6b95484fbbf9cf368de00ac1e
A.Sailesh Padmanabhan <arjun_sailesh@yahoo.com>
2021-06-18 12:11:46 +0530

    commit to remote

---
+++ SampleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCalculator
+{
+    class Program
+    {
+
+        public int add(int a,int b)
+        {
+            return a + b;
+        }
+        public int subtract(int a,int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
